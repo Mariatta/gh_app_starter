36db35c27550a3346d43bba18331bc35f8e4a55d
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2019-11-11 10:13:27 -0800

    Thanks the person who installed this bot

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -51,10 +51,28 @@ async def webhook(request):
 @router.register("installation", action="created")
 async def repo_installation_added(event, gh, *args, **kwargs):
     installation_id = event.data["installation"]["id"]
-    pass
+    installation_access_token = await utils.get_installation_access_token(
+        gh, installation_id
+    )
+    maintainer = event.data["sender"]["login"]
+    message = f"Thanks for installing me, @{maintainer}! (I'm a bot)."
+
+    for repository in event.data["repositories"]:
+        url = f"/repos/{repository['full_name']}/issues"
+        response = await gh.post(
+            url,
+            data={"title": "Mariatta's bot was installed", "body": message},
+            oauth_token=installation_access_token["token"],
+        )
+        issue_url = response["url"]
+        await gh.patch(
+            issue_url,
+            data={"state": "closed"},
+            oauth_token=installation_access_token["token"],
+        )
 
 
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":
     app = web.Application()
 
     app.router.add_routes(routes)
